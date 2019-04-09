5a8dc11fc0843fee656e9d655cc9506e4a191208
ShantnuS <shantnu@outlook.com>
2019-04-09 16:09:07 +0100

    getting a cors error on the request not really sure how to fix

--- js/utilities.js
+++ js/utilities.js
@@ -3,17 +3,31 @@ getBroadbandInfo("CO30RG");
 
 //Gets broadband information for a postcode
 function getBroadbandInfo(postcode){
-    $.ajax({ 
-        type : "GET", 
-        url : "https://api-proxy.ofcom.org.uk/broadband/coverage/"+postcode, 
-        beforeSend: function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', "key");},
-        success : function(result) { 
-            console.log(result);
-            //set your variable to the result 
-        }, 
-        error : function(result) {
-            console.log("ERROR"); 
-            console.log(result);
-        } 
-    }); 
+    // $.ajax({ 
+    //     url : "https://api-proxy.ofcom.org.uk/broadband/coverage/"+postcode, 
+    //     type : "get", 
+    //     crossDomain: true,
+    //     //dataType: "jsonp",
+    //     dataType: 'application/json',
+    //     beforeSend: function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', BROADBAND_KEY);},
+    //     success : function(result) { 
+    //         console.log(result);
+    //         //set your variable to the result 
+    //     }, 
+    //     error : function(result) {
+    //         console.log("ERROR"); 
+    //         console.log(result);
+    //     } 
+    // }); 
+    
+    $.ajaxSetup({
+        headers : {
+            "Ocp-Apim-Subscription-Key": BROADBAND_KEY
+        }
+    });
+
+    var url = "https://api-proxy.ofcom.org.uk/broadband/coverage/CO30RG";
+    $.getJSON(url, function(data) {
+        console.log(data);
+    });
 }
