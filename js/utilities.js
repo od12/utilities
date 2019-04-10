18d3a09f8d07523597889ed83bfb322fd362a526
ShantnuS <shantnu@outlook.com>
2019-04-10 13:31:10 +0100

    querying flask backend instead

--- js/utilities.js
+++ js/utilities.js
@@ -3,30 +3,15 @@ getBroadbandInfo("CO30RG");
 
 //Gets broadband information for a postcode
 function getBroadbandInfo(postcode){
-    // $.ajax({ 
-    //     url : "https://api-proxy.ofcom.org.uk/broadband/coverage/"+postcode, 
-    //     type : "get", 
-    //     crossDomain: true,
-    //     //dataType: "jsonp",
-    //     dataType: 'application/json',
-    //     beforeSend: function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', BROADBAND_KEY);},
-    //     success : function(result) { 
-    //         console.log(result);
-    //         //set your variable to the result 
-    //     }, 
-    //     error : function(result) {
-    //         console.log("ERROR"); 
-    //         console.log(result);
-    //     } 
-    // }); 
-    
-    $.ajaxSetup({
-        headers : {
-            "Ocp-Apim-Subscription-Key": BROADBAND_KEY
-        }
+    var url = "http://127.0.0.1:5000/broadband?postcode="+postcode;
+    $.getJSON(url, function(data) {
+        console.log(data);
     });
+}
 
-    var url = "https://api-proxy.ofcom.org.uk/broadband/coverage/CO30RG";
+//Gets broadband information for a postcode
+function getMobileInfo(postcode){
+    var url = "http://127.0.0.1:5000/mobile?postcode="+postcode;
     $.getJSON(url, function(data) {
         console.log(data);
     });
